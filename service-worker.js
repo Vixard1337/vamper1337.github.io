d90da1d290e2a6353e67051756b19a4b1418b01e
Vixard1337 <122740671+Vixard1337@users.noreply.github.com>
2024-05-19 12:27:55 +0200

    cache aded

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'cache_test';
+// List of files which are store in cache.
+let filesToCache = [
+ '/',
+ '/css/style.css',
+ '/js/script.js'
+];
+self.addEventListener('install', function (evt) {
+ evt.waitUntil(
+ caches.open(CACHE_NAME).then(function (cache) {
+ return cache.addAll(filesToCache);
+ }).catch(function (err) {
+ //console.error(err);
+ })
+ );
+});
+
+function isSuccessful(response) {
+    return response &&
+    response.status === 200 &&
+    response.type === 'basic';
+   }
+   self.addEventListener('fetch', function (event) {
+    event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+    if (response) {
+    return response; // Cache hit
+    }
+    return fetch(event.request.clone())
+    .then(function (response) {
+    if (!isSuccessful(response)) {
+    return response;
+    }
+    caches.open(CACHE_NAME)
+    .then(function (cache) {
+    cache.put(event.request, response.clone());
+    });
+    return response;
+    }
+    );
+    })
+    );
+   });
